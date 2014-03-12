55add663ca21caa7f5d17e2f95784ef924c2692e
mizugaki <k.t.mizugaki@gmail.com>
2014-03-12 21:24:51 +0900

    デーモン機能っぽいのを追加。

--- src/main.c
+++ src/main.c
@@ -6,16 +6,24 @@
  * Last Change: 23-Feb-2004.
  */
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/ioctl.h>
 
 #include "migemo.h"
 
 #define MIGEMO_ABOUT "cmigemo - C/Migemo Library " MIGEMO_VERSION " Driver"
 #define MIGEMODICT_NAME "migemo-dict"
 #define MIGEMO_SUBDICT_MAX 8
+
+int try_socket(const char *socket_path, const char *word);
+int create_socket(migemo *pmigemo, const char *socket_path);
 
 /*
  * main
@@ -59,6 +67,7 @@ help(char* prgname)
 USAGE: %s [OPTIONS]\n\
 \n\
 OPTIONS:\n\
+  -c --socket <socket>	Use path <socket> as socket.\n\
   -d --dict <dict>	Use a file <dict> for dictionary.\n\
   -s --subdict <dict>	Sub dictionary files. (MAX %d times)\n\
   -q --quiet		Show no message except results.\n\
@@ -79,6 +88,7 @@ main(int argc, char** argv)
     int mode_emacs = 0;
     int mode_nonewline = 0;
     int mode_quiet = 0;
+    char* socket = NULL;
     char* dict = NULL;
     char* subdict[MIGEMO_SUBDICT_MAX];
     int subdict_count = 0;
@@ -98,6 +108,8 @@ main(int argc, char** argv)
 	    mode_emacs = 1;
 	else if (!strcmp("--nonewline", *argv) || !strcmp("-n", *argv))
 	    mode_nonewline = 1;
+	else if (argv[1] && (!strcmp("--socket", *argv) || !strcmp("-c", *argv)))
+	    socket = *++argv;
 	else if (argv[1] && (!strcmp("--dict", *argv) || !strcmp("-d", *argv)))
 	    dict = *++argv;
 	else if (argv[1]
@@ -112,6 +124,13 @@ main(int argc, char** argv)
 	    help(prgname);
     }
 
+    if (socket && word) {
+	if (try_socket(socket, word)) {
+	    return 0;
+	}
+	/* if socket wasn't found, fallback to standalone */
+	socket = NULL;
+    }
 #ifdef _PROFILE
     fplog = fopen("exe.log", "wt");
 #endif
@@ -190,6 +209,10 @@ main(int argc, char** argv)
 		fprintf(fplog, mode_vim ? "%s" : "%s\n", ans);
 	    migemo_release(pmigemo, ans);
 	}
+	else if (socket)
+	{
+	    create_socket(pmigemo, socket);
+	}
 	else
 	{
 	    if (!mode_quiet)
@@ -219,3 +242,112 @@ main(int argc, char** argv)
 	fclose(fplog);
     return 0;
 }
+
+int try_socket(const char *socket_path, const char *word)
+{
+    int    fd;
+    struct sockaddr_un addr;
+    unsigned char buf[2048];
+
+    if ((fd = socket(PF_UNIX, SOCK_STREAM, 0)) < 0) {
+	perror("socket");
+	return 0;
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strcpy(addr.sun_path, socket_path);
+
+    if (connect(fd, (struct sockaddr *)&addr,
+	     sizeof(addr.sun_family) + strlen(addr.sun_path)) < 0){
+	perror("connect");
+	return 0;
+    }
+
+    int n = strlen(word)+1;
+    if (write(fd, word, n) != n) {
+	perror("write");
+	return 0;
+    }
+    while ((n = read(fd, buf, sizeof(buf)-1)) > 0) {
+	if (buf[n-1] == 0) {
+	    fwrite(buf, 1, n-1, stdout);
+	    break;
+	}
+	fwrite(buf, 1, n, stdout);
+    }
+    close(fd);
+
+    return 1;
+}
+
+int create_socket(migemo *pmigemo, const char *socket_path)
+{
+    int    fd1, fd2;
+    struct sockaddr_un saddr;
+    struct sockaddr_un caddr;
+    unsigned char buf[2048], *ans;
+
+    if ((fd1 = socket(PF_UNIX, SOCK_STREAM, 0)) < 0) {
+	perror("socket");
+	return 0;
+    }
+
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sun_family = AF_UNIX;
+    strcpy(saddr.sun_path, socket_path);
+
+    unlink(socket_path);
+    if (bind(fd1, (struct sockaddr *)&saddr,
+	     sizeof(saddr.sun_family) + strlen(saddr.sun_path)) < 0){
+	perror("bind");
+	return 0;
+    }
+
+    if (listen(fd1, 1) < 0) {
+	perror("listen");
+	return 0;
+    }
+
+    if (daemon(1, 0) == -1) {
+	perror("daemon");
+	return 0;
+    }
+
+    strcpy(buf, socket_path);
+    strcat(buf, ".pid");
+    pid_t pid = getpid();
+    FILE *fp = fopen(buf, "wb");
+    if (fp) {
+	fprintf(fp, "%lu", (unsigned long)pid);
+	fclose(fp);
+    }
+
+    int len = sizeof(caddr);
+    while (1)
+    {
+	if ((fd2 = accept(fd1, (struct sockaddr *)&caddr, (socklen_t *)&len)) < 0)
+	{
+	    perror("accept");
+	    return 0;
+	}
+	unsigned char byte;
+	size_t pos = 0;
+	while (pos < sizeof(buf)-1 && read(fd2, &byte, 1) > 0)
+	{
+	    if (byte == 0) break;
+	    buf[pos++] = byte;
+	}
+	buf[pos] = 0;
+	ans = migemo_query(pmigemo, buf);
+	if (ans) {
+	    write(fd2, ans, strlen(ans)+1);
+	}
+	migemo_release(pmigemo, ans);
+	close(fd2);
+    }
+    close(fd1);
+
+    return 0;
+}
+
